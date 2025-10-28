ab74fd8a20d6c8ddff183b6f5a8cdf3cc7d3785c
Marco Rojouan <mrojouan@3F4.42angouleme.fr>
2025-10-28 17:14:05 +0100

    bizarre

--- get_next_line.c
+++ get_next_line.c
@@ -3,55 +3,89 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loup <loup@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mrojouan <mrojouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 15:41:20 by loup              #+#    #+#             */
-/*   Updated: 2025/10/27 18:22:11 by loup             ###   ########.fr       */
+/*   Updated: 2025/10/28 17:12:29 by mrojouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdlib.h>
-#include <fcntl.h>
-# define BUFFER 5
 
-int ft_strlen(char *str)
+char *get_buffer(char *buffer, int fd)
 {
-    int i;
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
+    int read_count;
+    char error2[] = "erreur de lecture";
+
+    read_count = 0;
+    read_count = read(fd, buffer, BUFFER);
+    if (read_count == -1)
+    {    
+        write(2, error2 , ft_strlen(error2));
+        return (NULL);
+    }
+    buffer[read_count] = '\0';
+    return (buffer);
+}
+
+char *get_next_line(int fd)
+{
+    char *buffer;
+
+    buffer = malloc(sizeof(char) * (BUFFER + 1));
+    if (!buffer)
+        return (NULL);
+    if (fd < 0)
+        return (NULL);
+    if (BUFFER <= 0)
+        return (NULL);
+    buffer = get_buffer(buffer, fd);
+    return (buffer);
 }
 
 int main(void)
 {
-    char error1[] = "erreur avec le fichier";
-    char error2[] = "erreur de lecture";
-    char c;
-    unsigned long occ;
-    // char buffer[BUFFER];
-    unsigned long count;
-    int newfd;
-    
-    occ = 0;
-    newfd = open("file.txt", O_RDONLY);
-    if (newfd == -1)
+    char *buffer;
+    int len;
+    int fd = open("file.txt", O_RDONLY);
+    buffer = get_next_line(fd);
+    len = ft_strlen(buffer);
+    if (!buffer)
     {
-        write(2, error1 , ft_strlen(error1));
-        return (newfd);
+        free(buffer);
+        return (0);
     }
-    while ((count = read(newfd, &c, 1)) > 0)
-    {
-        if (c == 'a')
-            occ++;
-        write(1, &c, count);
-    }
-    if (count == -1)
-    {
-        write(2, error2, ft_strlen(error2));
-        return (count);
-    }
-    close(newfd);
-    return (occ);
+    write(2, buffer, len);
+    free(buffer);
 }
+// int main(void)
+// {
+//     char error1[] = "erreur avec le fichier";
+//     char error2[] = "erreur de lecture";
+//     char c;
+//     unsigned long occ;
+//     // char buffer[BUFFER];
+//     unsigned long count;
+//     int newfd;
+    
+//     occ = 0;
+//     newfd = open("file.txt", O_RDONLY);
+//     if (newfd == -1)
+//     {
+//         write(2, error1 , ft_strlen(error1));
+//         return (newfd);
+//     }
+//     while ((count = read(newfd, &c, 1)) > 0)
+//     {
+//         if (c == 'a')
+//             occ++;
+//         write(1, &c, count);
+//     }
+//     if (count == -1)
+//     {
+//         write(2, error2, ft_strlen(error2));
+//         return (count);
+//     }
+//     close(newfd);
+//     return (occ);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -3,16 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loup <loup@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mrojouan <mrojouan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 16:50:21 by loup              #+#    #+#             */
-/*   Updated: 2025/10/27 16:51:02 by loup             ###   ########.fr       */
+/*   Updated: 2025/10/28 16:41:47 by mrojouan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER 500
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+char	*ft_strjoin(const char *s1, const char *s2);
+int	ft_strlen(char *str);
 
 # endif
